a4c7bca1c62e9e2461f939c1359db70f4e9ab239
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-12-19 22:57:42 +0300

    add redis connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import {RedisModule} from 'nestjs-redis';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    RedisModule.register([
+        {
+          name: process.env.REDIS_CONNECTION_NAME,
+          url: process.env.REDIS_CONNECTION
+        }
+    ])
   ],
   controllers: [AppController],
   providers: [AppService],
