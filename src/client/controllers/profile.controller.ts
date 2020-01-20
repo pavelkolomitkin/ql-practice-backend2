77f6309952511df1c02daec900bff26b1395fea7
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2020-01-21 02:47:08 +0300

    add prifile edition

--- src/client/controllers/profile.controller.ts
+++ src/client/controllers/profile.controller.ts
@@ -1,8 +1,28 @@
-import {Controller, UseGuards} from '@nestjs/common';
+import {Body, Controller, Put, UseGuards} from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
+import {ProfileDto} from '../dto/profile.dto';
+import {User as CurrentUser} from '../../core/decorators/user.decorator';
+import {ClientUser} from '../../entity/models/client-user.entity';
+import {InjectEntityManager} from '@nestjs/typeorm';
+import {EntityManager} from 'typeorm';
 
 @UseGuards(AuthGuard())
-@Controller('profile')
+@Controller('client/profile')
 export class ProfileController {
 
+    constructor(
+        @InjectEntityManager()
+        private readonly entityManager: EntityManager
+    ) {
+    }
+
+    @Put('/')
+    async edit(@Body() data: ProfileDto, @CurrentUser('client') user: ClientUser)
+    {
+        user.aboutMe = data.aboutMe.trim();
+        user.fullName = data.fullName.trim();
+
+        await this.entityManager.save(user);
+        return user.serialize(['mine']);
+    }
 }
